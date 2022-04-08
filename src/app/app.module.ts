0ed6887aea9b6dbb99d44b468273897dc7a9055b
Moses Karani <karanim594@gmail.com>
2022-04-08 16:58:46 +0300

    created a quote details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './components/quote/quote.component';
+import { QuoteDetailsComponent } from './components/quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
+    QuoteDetailsComponent,
   ],
   imports: [
     BrowserModule,
