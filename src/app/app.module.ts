32cb31dc71eb9457e8af32228fcca1bbef182380
Moses Karani <karanim594@gmail.com>
2022-04-08 17:28:15 +0300

    created a date-count custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './components/quote/quote.component';
 import { QuoteDetailsComponent } from './components/quote-details/quote-details.component';
+import { DateCountPipe } from './pipe/date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
+    DateCountPipe,
   ],
   imports: [
     BrowserModule,
