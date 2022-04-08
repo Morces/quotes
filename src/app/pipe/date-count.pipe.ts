8cca0778e20d00acdc7e591a342573861feec483
Moses Karani <karanim594@gmail.com>
2022-04-08 17:36:54 +0300

    optimized the date pipe

--- src/app/pipe/date-count.pipe.ts
+++ src/app/pipe/date-count.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate());
+    var dateDifference = Math.abs(todayWithNoTime - value);
+    const secondsInDay = 86400; // 60sec a min * 60min an hr * 24hrs a day
+
+    var dateDifferenceSeconds = dateDifference*0.001; // convert miliseconds
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+
+    if (dateCounter >= 1 && todayWithNoTime > value) {
+      return dateCounter;
+    }else {
+      return 0;
+    }
   }
 
 }
